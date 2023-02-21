0a40d2a7ad9096acd5ed605bd4386dffffe10a54
Vlad Ten <vlad010694@gmail.com>
2023-02-21 21:56:20 +0100

    upd: template

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import "./App.css";
 import { TonConnectButton } from "@tonconnect/ui-react";
-import { Counter } from "./components/Counter";
-import { Jetton } from "./components/Jetton";
 import { TransferTon } from "./components/TransferTon";
 import styled from "styled-components";
 import { Button, FlexBoxCol, FlexBoxRow } from "./components/styled/styled";
@@ -43,9 +41,7 @@ function App() {
                 : "N/A"}
             </Button>
           </FlexBoxRow>
-          <Counter />
           <TransferTon />
-          <Jetton />
         </FlexBoxCol>
       </AppContainer>
     </StyledApp>
